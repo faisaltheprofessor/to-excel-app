10fd7684b9e0b3a1d6677b8ad92097b8c2af452f
Faisal Khan <faisalfazily@gmail.com>
2025-12-19 12:15:51 +0100

    remove debug bar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-        $loader->alias('Debugbar', \Barryvdh\Debugbar\Facades\Debugbar::class);
     }
 
     /**
